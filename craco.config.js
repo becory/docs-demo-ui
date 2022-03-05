d13bcab3428f76fdb328581b39e09cf41bc9d9bf
becory <13202145+becory@users.noreply.github.com>
2022-03-05 23:19:35 +0800

    chore: config

--- craco.config.js
+++ craco.config.js
@@ -7,4 +7,16 @@ module.exports = {
             ],
         },
     },
+    webpack: {
+        configure: (webpackConfig, {
+            env, paths
+        }) => {
+            webpackConfig.output = {
+                ...webpackConfig.output,
+                // path: path.resolve(__dirname, 'build'), // 修改输出文件目录
+                publicPath: '../'
+            }
+            return webpackConfig
+        }
+    }
 }
